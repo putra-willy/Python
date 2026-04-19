9cc7196adce319b66ef40044ea099943b63f80b3
putra-willy <putrawilly442@gmail.com>
2026-04-19 20:17:04 +0700

    Add files via upload
    cek kelulusan

---
+++ belajar.py
@@ -0,0 +1,9 @@
+def cek_kelulusan(nilai):
+    if nilai > 75:
+        return "Lulus"
+    elif nilai < 75:
+        return "Tidak Lulus"
+    else:
+        return "Lulus"
+nilai = int(input("Masukan nilai anda = "))
+print(cek_kelulusan(nilai))
